0712e59b56ecf40f5e282fb16305e5c5d9e845ce
Ian Cameron and Mavroian Florin <ian.kameron+mavflorin@gmail.com>
2019-04-19 18:04:47 +0900

    started assignment

--- test/test.js
+++ test/test.js
@@ -1,12 +1,46 @@
 const { expect } = require("chai");
 const Ghibliator = require("../Ghibliator");
+console.log(require("sinon"));
+
+const justAges = [
+  { age: "late teens" },
+  { age: "17" },
+  { age: "Unspecified/Adult" },
+  { age: "Middle age" },
+  { age: "Adult" },
+  { age: "Adult" },
+  { age: "Over 50" },
+  { age: "Unknown" },
+  { age: "400" },
+  { age: "300" },
+  { age: "NA" },
+  { age: "11" },
+  { age: "4" },
+  { age: "37" },
+  { age: "Adult" },
+  { age: "Elder" },
+  { age: "11" },
+  { age: "" },
+  { age: "NA" },
+  { age: "NA" },
+  { age: "NA" },
+  { age: "87" },
+  { age: "NA" },
+  { age: "13" },
+  { age: "NA" },
+  { age: "NA" },
+  { age: "33" },
+  { age: "47" },
+  { age: "5" },
+];
 
 describe("Ghibliator", () => {
   it("should return average age of characters", () => {
     // setup
     const expected = 33;
     const delta = 1;
-
+    const fileAccess = sinon.stub(fs, "readFileSync").returnsThis(justAges);
+    fileAccess.y;
     // exercise
     const actual = new Ghibliator().getAverageAge();
 
