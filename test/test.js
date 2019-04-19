d2cf17354148e882a29037c7b753faf899f8b5fb
mavroian <mavflorin@gmail.com>
2019-04-19 23:41:58 +0900

    Test-doubles Finished

--- test/test.js
+++ test/test.js
@@ -1,65 +1,45 @@
 const { expect } = require("chai");
 const Ghibliator = require("../Ghibliator");
-console.log(require("sinon"));
-
-const justAges = [
-  { age: "late teens" },
-  { age: "17" },
-  { age: "Unspecified/Adult" },
-  { age: "Middle age" },
-  { age: "Adult" },
-  { age: "Adult" },
-  { age: "Over 50" },
-  { age: "Unknown" },
-  { age: "400" },
-  { age: "300" },
-  { age: "NA" },
-  { age: "11" },
-  { age: "4" },
-  { age: "37" },
-  { age: "Adult" },
-  { age: "Elder" },
-  { age: "11" },
-  { age: "" },
-  { age: "NA" },
-  { age: "NA" },
-  { age: "NA" },
-  { age: "87" },
-  { age: "NA" },
-  { age: "13" },
-  { age: "NA" },
-  { age: "NA" },
-  { age: "33" },
-  { age: "47" },
-  { age: "5" },
-];
+const fs = require("fs");
+const sinon = require("sinon");
 
 describe("Ghibliator", () => {
   it("should return average age of characters", () => {
     // setup
+    const fakeObj = JSON.stringify([{ age: 33 }]);
     const expected = 33;
     const delta = 1;
-    const fileAccess = sinon.stub(fs, "readFileSync").returnsThis(justAges);
-    fileAccess.y;
-    // exercise
-    const actual = new Ghibliator().getAverageAge();
 
+    //exercise
+    const fileSistem = sinon.stub(fs, "readFileSync").returns(fakeObj);
+    const actual = new Ghibliator().getAverageAge();
     // assert
     expect(actual).closeTo(actual, expected, delta);
 
     // teardown
+    fileSistem.restore();
   });
 
   it("should return appearances", () => {
     // setup
     const expected = 2;
-
+    const fakeObj = JSON.stringify([
+      {
+        name: "Baron Humbert von Gikkingen",
+        films: [
+          "https://ghibliapi.herokuapp.com/films/ff24da26-a969-4f0e-ba1e-a122ead6c6e3",
+          "https://ghibliapi.herokuapp.com/films/90b72513-afd4-4570-84de-a56c312fdf81",
+        ],
+      },
+    ]);
     // exercise
+    const fileSistem = sinon.stub(fs, "readFileSync").returns(fakeObj);
     const actual = new Ghibliator().appearances("Baron Humbert von Gikkingen");
 
     // assert
     expect(actual).to.equal(actual, expected);
 
     // teardown
+    fileSistem.restore();
   });
 });
